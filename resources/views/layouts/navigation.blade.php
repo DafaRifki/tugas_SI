73cd805617a862b4bb37a41f5e0151c498c63825
MAULANA <maulana248adam@gmail.com>
2024-10-31 14:36:39 +0700

    Halaman Daftar Barang

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('daftarbarang')" :active="request()->routeIs('dashboard')">
+                        {{ __('Daftar Barang') }}
+                    </x-nav-link>
                 </div>
             </div>
 
